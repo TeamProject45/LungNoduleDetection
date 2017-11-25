45209efa112c7a474f51bdbe90ded772d3718855
TUNASANG-NB\tunas <tunasang_18@hotmail.com>
2017-11-25 15:21:03 +0700

    fix some bug

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ function FilterTable() {
     filter = input.value.toLowerCase();
     table = document.getElementById("data_table");
     tr = table.getElementsByTagName("tr");
-  
+
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            if (td.innerHTML.toLowerCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
-      } 
     }
-  }
+}
